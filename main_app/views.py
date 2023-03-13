e059a495008bc176c984915ac35c3e66ddc5724d
Kristone Brown <Keyystone4@gmail.com>
2023-03-13 14:44:53 -0500

    First Commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+finches = [
+  {'name': 'Wings', 'breed': 'House finch', 'description': 'small-bodied finches with fairly large beaks', 'age': 4},
+  {'name': 'Tweet', 'breed': 'Pyrrhula', 'description': 'distinctive black cap, rose-red underparts, and white rump.', 'age': 6},
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+    'finches': finches
+
+    })
