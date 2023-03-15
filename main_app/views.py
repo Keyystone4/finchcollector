bc92a7b2c212466ff2de3d9a24d2d24a18b71bdf
Kristone Brown <Keyystone4@gmail.com>
2023-03-15 14:10:36 -0500

    first commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
-
-finches = [
-  {'name': 'Wings', 'breed': 'House finch', 'description': 'small-bodied finches with fairly large beaks', 'age': 4},
-  {'name': 'Tweet', 'breed': 'Pyrrhula', 'description': 'distinctive black cap, rose-red underparts, and white rump.', 'age': 6},
-]
 
 def home(request):
     return render(request, 'home.html')
@@ -14,7 +12,32 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
-    return render(request, 'finches/index.html', {
-    'finches': finches
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', { 'finches': finches})  
 
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch, 'feeding_form': feeding_form
     })
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+        return redirect('detail', finch_id=finch_id)
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
